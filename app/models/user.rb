74502503005d7dac8f80ba02a04732f57c5b0d29
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-11-07 10:47:51 +1100

    remove methods from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,4 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  has_one :store
-  
-  after_create :create_the_store
-
-
-  def create_the_store
-    name = generate_store_name
-    qr_code_img = RQRCode::QRCode.new('http://www.google.com/', :size => 16, :level => :h ).to_img
-
-    store = self.create_store(name:generate_store_name)
-    store.update_attribute :qr_code, qr_code_img.to_string
-
-    Product.all.each { |p| store.products << p } 
-  end
-
-  def generate_store_name
-    existing_names = Store.pluck(:name)
-    name = email[0..email.index('@')-1]
-    while(existing_names.include?(name)) do
-      name += '1'
-    end 
-    name
-  end
 end
